b4623c7b5e474028adfe8cd455f1b235f1d77e1b
gimenados <gimenasande1@gmail.com>
2023-12-11 20:49:06 -0300

    retoques

--- js/script.js
+++ js/script.js
@@ -6,15 +6,6 @@ const cssFiles = [
 let activeFile = 0;
 const links = document.getElementsByTagName("link");
 
-// Verifica si hay un estilo recordado en el localStorage al cargar la página
-document.addEventListener("DOMContentLoaded", () => {
-  const recordadoIndex = localStorage.getItem("recordadoIndex");
-  if (recordadoIndex !== null) {
-    activeFile = parseInt(recordadoIndex, 10);
-    links[0].href = cssFiles[activeFile];
-  }
-});
-
 function onClick() {
   activeFile++;
   if (activeFile > 2) {
@@ -24,20 +15,8 @@ function onClick() {
   console.log(cssFiles[activeFile]);
 }
 
-function resetForm() {
-  var form = document.getElementById("miForm");
-  var currentIndex = activeFile;
-  var currentStyle = links[0].href;
-  form.setAttribute("novalidate", true);
-  form.reset();
-  setTimeout(function () {
-    form.removeAttribute("novalidate");
-    activeFile = currentIndex;
-    links[0].href = currentStyle;
-  }, 100);
-}
-
 function recordarEstilo() {
+  console.log("Haciendo clic en Recordar");
   const recordarButton = document.getElementById("recordarButton");
   recordarButton.classList.toggle("active");
 
